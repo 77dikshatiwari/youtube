52543cf2fcfcdfbe016964e8985e95fab3c50ba6
fundfixrs <praveen.tiwari@fundfixr.com>
2024-03-11 10:41:14 +0530

    utills

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// const asuncHandler2 = ()=> {}
+// const asyncHandler2 = (func) => () => {}
+// const asyncHandler2 = (func) => async () => {}
+
+// const asyncHandler1 = (fn) => async (req, res, next) => {
+//  try {
+//     await fn(req, res, next)
+//  } catch (error) {
+//     res.status(error.code || 500).json({success: false, message: error.message})
+//  }
+// }
