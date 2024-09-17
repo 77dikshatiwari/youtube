ef29a20dc36401c561afcbe211f9f97b2d6e0eae
77dikshatiwari <diksha1995tiwari@gmail.com>
2024-09-17 16:43:55 +0530

    spelling error

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -86,4 +86,4 @@ userSchema.methods.generateRefreshToken = function () {
     )    
 }
 
-export const User = mongoose.model("user", userSchema);
+export const User = mongoose.model("User", userSchema);
--- src/models/videoModel.js
+++ src/models/videoModel.js
@@ -27,7 +27,7 @@ const videoSchema = new Schema(
       type: Number,
       default: 0,
     },
-    isPuublished: {
+    isPublished: {
       type: Boolean,
       default: true,
     },
